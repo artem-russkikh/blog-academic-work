f5ee8cd891ae9d1037cb2cea7b0eaadef8d410f9
khohryakov <khohryakovmax@gmail.com>
2018-02-23 19:11:50 +0500

    Finish main page makeup

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import 'moment/locale/ru';
+import moment from 'moment';
 
+moment.locale('ru')
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
