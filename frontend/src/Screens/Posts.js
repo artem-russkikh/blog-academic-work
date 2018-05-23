17af06045458d755980d6d1be8270d6a769ed332
khohryakov <khohryakovmax@gmail.com>
2018-05-23 22:22:50 +0500

    Update Posts:Get client code

--- frontend/src/Screens/Posts.js
+++ frontend/src/Screens/Posts.js
@@ -1,18 +1,18 @@
 import React from 'react';
-import Card, {CardActions, CardContent, CardMedia} from 'material-ui/Card';
+import Card, { CardActions, CardContent, CardMedia } from 'material-ui/Card';
 import Button from 'material-ui/Button'
 import Typography from 'material-ui/Typography';
 import Grid from 'material-ui/Grid';
 import { FormControlLabel } from 'material-ui/Form';
 import Switch from 'material-ui/Switch';
 import moment from 'moment';
-import {Link} from 'react-router';
-import data from '../data'
+import { Link } from 'react-router';
+import axios from 'axios';
 
 const formattedDescription = (description) => {
   return description && description.length > 120 ?
-           `${description.substring(0, 120)}...` :
-           description
+    `${description.substring(0, 120)}...` :
+    description
 }
 
 const formattedCreatedAt = (createdAt) => {
@@ -20,12 +20,35 @@ const formattedCreatedAt = (createdAt) => {
 }
 
 export default class Posts extends React.Component {
-  constructor (props) {
+  constructor(props) {
     super(props)
 
     this.state = {
-      onlyMyPosts: false
+      my_id: 1,
+      onlyMyPosts: false,
+      data: null,
+      stub: 'Пока что записей нет.'
     }
+  }
+
+  componentDidMount() {
+    axios.get(`http://127.0.0.1:5000/posts.json`, {params: { limit: 20, offset: 0 }})
+      .then(res => {
+        const errCode = res.data['error']['code'];
+
+        if (errCode !== 0) {
+          this.setState({stub:'Ошибка сервера'});
+          return;
+        }
+
+        const post_data = res.data['result'];
+        
+        if(post_data.length != 0)
+          this.setState({data: post_data});
+      })
+      .catch(err => {
+          this.setState({stub:'Ошибка ' + err});
+      });
   }
 
   toggleSwitch = (_e) => {
@@ -35,9 +58,22 @@ export default class Posts extends React.Component {
   }
 
   render() {
-    return (
-      <div className="container">
-        <FormControlLabel
+    return this.state.data == null ?
+      (
+        <div className="container">
+          <Grid container>
+            <Grid item xs={12}>
+              <Typography variant='alignCenter'>
+                {this.state.stub}
+                </Typography>
+            </Grid>
+          </Grid>
+        </div>
+      )
+      :
+      (
+        <div className="container">
+          <FormControlLabel
             control={
               <Switch
                 checked={this.state.onlyMyPosts}
@@ -48,45 +84,46 @@ export default class Posts extends React.Component {
             }
             label="Показывать только мои записи"
           />
-        <Grid container spacing={24}>
-          {data.map((el, idx) => {
-            return (
-              <Grid item xs={12} sm={6} key={idx}>
-                <Card style={{ maxWidth: 450 }}>
-                  <CardMedia
-                    style={{
-                      height: 300,
-                      backgroundPosition: 'center top',
-                    }}
-                    image={el.image}
-                  />
-                  <CardContent style={{ minHeight: 92 }}>
-                    <Typography variant="headline" component="h2">
-                      {el.title}
-                    </Typography>
-                    <Typography component="p">
-                      {formattedDescription(el.description)}
-                    </Typography>
-                  </CardContent>
-                  <CardActions>
-                    <Button
-                      size="small"
-                      color="primary"
-                      component={Link}
-                      to={`/posts/${el.id}`}
-                    >
-                      Подробнее
+          <Grid container spacing={24}>
+            {this.state.data.map((post, idx) => {
+              {if(this.state.onlyMyPosts && post.author_id != this.state.my_id) return; }
+              return (
+                <Grid item xs={12} sm={6} key={idx}>
+                  <Card style={{ maxWidth: 450 }}>
+                    <CardMedia
+                      style={{
+                        height: 300,
+                        backgroundPosition: 'center top',
+                      }}
+                      image={post.image}
+                    />
+                    <CardContent style={{ minHeight: 92 }}>
+                      <Typography variant="headline" component="h2">
+                        {post.title}
+                      </Typography>
+                      <Typography component="p">
+                        {formattedDescription(post.description)}
+                      </Typography>
+                    </CardContent>
+                    <CardActions>
+                      <Button
+                        size="small"
+                        color="primary"
+                        component={Link}
+                        to={`/posts/${post.id}`}
+                      >
+                        Подробнее
                     </Button>
-                    <Typography title={moment(el.createdAt).format('lll')} style={{marginLeft: 'auto'}}>
-                      {formattedCreatedAt(el.createdAt)}
-                    </Typography>
-                  </CardActions>
-                </Card>
-              </Grid>
-            )
-          })}
-        </Grid>
-      </div>
-    );
+                      <Typography title={moment(post.created_at).format('lll')} style={{ marginLeft: 'auto' }}>
+                        {formattedCreatedAt(post.created_at)}
+                      </Typography>
+                    </CardActions>
+                  </Card>
+                </Grid>
+              )
+            })}
+          </Grid>
+        </div>
+      );
   }
 }
